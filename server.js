562f3ed5ccfcf0fa584c6a23455f02ef72295e0f
Nathan <nathann@email.unc.edu>
2023-03-07 16:45:15 -0500

    Finished server.js code pt6.

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ if (err) {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
 	res.end(data);
 });
 
